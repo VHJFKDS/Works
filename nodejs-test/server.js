2a7c41560c07d63087bac76b2a980e7bc00380e3
Yang <651033755@qq.com>
2020-06-05 18:01:44 +0800

    add a new nodejs-test

---
+++ nodejs-test/server.js
@@ -0,0 +1,57 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
+
+  if(path === '/'){      //如果用户请求的路径是根目录
+    var string = fs.readFileSync('./index.html','utf8')
+    var amount = fs.readFileSync('./db','utf8')
+    string = string .replace('&&&amount&&&',amount)
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(string)
+    response.end()
+  }else if(path === '/server.js'){
+    var string = fs.readFileSync('./server.js','utf8')
+    response.setHeader('Content-Type', 'application/javascrip')
+    response.write(string)
+    response.end()
+  }else if(path === '/pay' && method.toUpperCase() === 'POST'){
+    var amount = fs.readFileSync('./db','utf8') //100
+    var newAmount = amount - 1
+    fs.writeFileSync('./db',newAmount)
+    response.write('success')
+    response.end()
+  }else{
+    response.statusCode = 400
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('找不到对应路径，你需要自行修改index.js')
+    response.end()
+  }
+
+
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
+
