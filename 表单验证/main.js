9f1d8bcdbb30de9086baabd3e094fa09687bf346
Yang <651033755@qq.com>
2020-07-12 17:41:45 +0800

    完整的注册表单验证

--- 表单验证/main.js
+++ 表单验证/main.js
@@ -18,33 +18,91 @@ function showSuccess(input){
     formControl.className = 'form-control success'
 }
 //check email is valid
-function isValidEmail(email){
+function checkEmail(input){
     const re = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
-    return re.test(String(email))
+    if(re.test(input.value.trim())){
+        showSuccess(input)
+    }else{
+        showError(input,'邮箱格式错误')
+    }
 }
+
+//checkRequired input
+function checkRequired(inputArr){
+   inputArr.forEach(function(input){    //遍历input标签，获取四个的value值，
+       if(input.value.trim() ===''){    //trim()去掉头尾空格
+           showError(input,`${input.placeholder}为必填项`)
+       }else{
+           showSuccess(input)
+       }
+   });
+}
+
+//checkLength
+function checkLength(input,min,max){
+    if(input.value.length < min){
+        showError(input,`${input.placeholder}至少需要${min}个字符`)
+    }else if(input.value.length > max){
+        showError(input,`${input.placeholder}不得超过${max}个字符`)
+    }else{
+        showSuccess(input)
+    }
+}
+//checkPasswordsMatch
+function checkPasswordsMatch(input1,input2){
+    if(input1.value !== input2.value){
+        showError(input2,'密码不匹配')
+    }
+}
+
 //event listener
 form.addEventListener('submit',function(e){
-    e.preventDefault()    //取消默认刷新
-    if(username.value === ''){
-        showError(username,'用户名为必填项')
-    }else{
-        showSuccess(username)
-    }
-    if(email.value === ''){
-        showError(email,'邮箱为必填项')
-    }else if(!isValidEmail(email.value)){   //如果不符合格式
-        showError(email,'邮箱格式错误')
-    }else{
-        showSuccess(email)
-    }
-    if(password.value === ''){
-        showError(password,'密码为必填项')
-    }else{
-        showSuccess(password)
-    }
-    if(password2.value === ''){
-        showError(password2,'确认密码为必填项')
-    }else{
-        showSuccess(password2)
-    }
+    e.preventDefault()   
+    checkRequired([username,email,password,password2])
+    // checkRequired(username)
+    // checkRequired(email)
+    // ...
+    checkLength(username,3,15)
+    checkLength(password,6,12)
+    checkEmail(email)
+    checkPasswordsMatch(password,password2)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //event listener
+// form.addEventListener('submit',function(e){
+//     e.preventDefault()    //取消默认刷新
+//     if(username.value === ''){
+//         showError(username,'用户名为必填项')
+//     }else{
+//         showSuccess(username)
+//     }
+//     if(email.value === ''){
+//         showError(email,'邮箱为必填项')
+//     }else if(!isValidEmail(email.value)){   //如果不符合格式
+//         showError(email,'邮箱格式错误')
+//     }else{
+//         showSuccess(email)
+//     }
+//     if(password.value === ''){
+//         showError(password,'密码为必填项')
+//     }else{
+//         showSuccess(password)
+//     }
+//     if(password2.value === ''){
+//         showError(password2,'确认密码为必填项')
+//     }else{
+//         showSuccess(password2)
+//     }
+// })
