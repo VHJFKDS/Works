be3a88b3098d9d67894c3bf3d9818868d9a70b2c
Yang <651033755@qq.com>
2020-07-27 14:44:12 +0800

    本地存储

--- 在线影院选票/main.js
+++ 在线影院选票/main.js
@@ -5,12 +5,19 @@ const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
 let ticketPrice = +movieSelect.value   //变成number
 
+populateUI()
 //更新座位数及总票价
 function undateSeletedCount(){ 
     const selectedSeats = document.querySelectorAll('.row .seat.selected')   //选取所有被点击的座位
+
     //下面获取索引数组
-    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
-    //本地存储
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))  //通过map返回新数组
+    // const seatsIndex = [...selectedSeats].map(function(seat){
+    //     return [...seats].indexOf(seat)  //筛选出第一次出现的索引值
+    // })
+
+    //将获得的索引值进行本地存储
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex))  //运用json来变成字符串
 
     localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex))
     const selectedSeatsCount = selectedSeats.length  //选中座位计数
@@ -21,7 +28,22 @@ function undateSeletedCount(){
 function setMovieData(movieIndex,moviePrice){
     localStorage.setItem('selectedMovieIndex',movieIndex)
     localStorage.setItem('selectedMoviePrice',moviePrice)
+}
 
+//获取本地数据并渲染样式
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+    if(selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach((seat,index)=>{
+            if(selectedSeats.indexOf(index)>-1){  //如果本地索引值出现的话
+                 seat.classList.add('selected')
+            }
+        })
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+    if(selectedMovieIndex !== null){
+        movieSelect.selectedIndex = selectedMovieIndex
+    }
 }
 //电影下拉框事件监听
 movieSelect.addEventListener('change',e=>{
@@ -37,3 +59,6 @@ container.addEventListener('click',e=>{   //选中整个座位区域
         undateSeletedCount()
     }
 })
+
+//设置初始座位和总票价
+undateSeletedCount()
