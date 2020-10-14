b77a5c683633296581b518ffedaef03d15d4a6cc
Yang <651033755@qq.com>
2020-10-14 22:27:07 +0800

    记账本-更新余额和收支

--- JS-Practice/记账本/main.js
+++ JS-Practice/记账本/main.js
@@ -1,6 +1,6 @@
 const balance = document.getElementById('balance')
-const money_plus = document.getElementById('money_plus')
-const money_minus = document.getElementById('money_minus')
+const money_plus = document.getElementById('money-plus')
+const money_minus = document.getElementById('money-minus')
 const list = document.getElementById('list')
 const form = document.getElementById('form')
 const text = document.getElementById('text')
@@ -32,9 +32,35 @@ function addTransactionDOM(transaction){
     list.appendChild(item)
 }
 
+//更新余额，收入，支出的金额
+function updateValues(){
+    //通过map获得交易金额数组
+    const amounts = transactions.map(transaction =>transaction.amount)
+
+    //reduce（）方法得到余额
+    const total = amounts.reduce((acc,item)=>(acc+=item),0).toFixed(2)
+
+    //filter过滤reduce累加   收入
+    const income = amounts
+    .filter(item=>item>0)
+    .reduce((acc,item)=>(acc+=item),0)
+    .toFixed(2)
+
+    //filter过滤reduce累加   支出
+    const expense = (amounts
+    .filter(item=>item<0)
+    .reduce((acc,item)=>(acc+=item),0)*-1)
+    .toFixed(2)
+    
+    balance.innerText = `$${total}`
+    money_plus.innerText = `$${income}`
+    money_minus.innerText =`$${expense}`
+}
+
 //初始化应用
 function init(){
     list.innerHTML = ''
     transactions.forEach(addTransactionDOM)
+    updateValues()
 }
 init()
